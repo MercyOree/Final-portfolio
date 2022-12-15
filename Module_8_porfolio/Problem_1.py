e2d96d33066dd759af8c6e55afa1ce7b91d7ac89
MercyOree <115586010+MercyOree@users.noreply.github.com>
2022-12-14 23:32:47 -0600

    Add files via upload

---
+++ Module_8_porfolio/Problem_1.py
@@ -0,0 +1,14 @@
+# Mercy O. koku
+# Nov/29/2022
+# Problem 1 is about how to write a function that takes two inputs from a user.
+# Prints whether they are equal or not.
+
+def is_equal(num1, num2):
+    return len(X) == len(Y)
+
+X = input("Enter a first number ")
+Y = input("Enter a second number ")
+if (X) == (Y):
+    print("Numbers are equal to each other ")
+else:
+    print("Numbers are not equal to each other ")
